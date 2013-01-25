50f1ab8a5a9eb58998d50760aefb66be9b37987c
Mathieu Bolla <mbolla@splayce.com>
2013-01-25 11:42:07 +0100

    Benchmarking on 4 runs for the various beta angles

---
+++ bench.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sum="0.0"
+for beta in "-72" "-71" "71" "72"
+do
+    javac src/ISS.java && java -jar resources/ISSVis.jar -exec "java -cp src/ ISS" -model resources/ISS_simple.model -beta $beta > results.dat
+    compute="$sum + `grep \"Score\" results.dat | cut -d " " -f 3`"
+    echo $compute
+    sum=`echo "$compute" | bc`
+done
+echo "$sum / 4" | bc
+rm -rf results.dat
