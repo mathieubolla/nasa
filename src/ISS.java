d0a85546cc5a249c39117ff05a2f4644281b5928
Mathieu Bolla <mbolla@splayce.com>
2013-01-25 09:56:01 +0100

    First revision, not working

---
+++ src/ISS.java
@@ -0,0 +1,336 @@
+import java.io.*;
+
+public class ISS {
+	public static final double SARJ_VELOCITY_LIMIT = 0.150; // per second
+	public static final double SARJ_VELOCITY_HALF = 0.075; // per second
+	//public static final double SARJ_ACCELERATION_LIMIT = 0.005; // per second, 0.3 per minute
+
+	public static final double BGA_VELOCITY_LIMIT = 0.250; // per second
+	public static final double BGA_VELOCITY_HALF = 0.125; // per second
+	//public static final double BGA_ACCELERATION_LIMIT = 0.01; // per second
+
+	public static final Positions INITIAL_POSITION = new Positions(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+	public static final Speeds MINUMUM_SPEED = new Speeds(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+
+	public static void main(String... args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		ISS iss = new ISS();
+
+		String beta = in.readLine();
+		System.out.println(iss.getInitialOrientation(Double.parseDouble(beta)));
+		System.out.flush();
+
+		for (int minute = 0; minute <= 91; minute++) {
+			System.out.println("1");
+			System.err.println("at "+minute);
+			int i = 0;
+			for (double angle : iss.getStateAtMinute(minute)) {
+				if (i++ % 2 == 1) {
+					angle = 0.0;
+				}
+				System.out.println(roundedDouble(angle));
+				System.err.print(roundedDouble(angle) + " ");
+			}
+			System.err.println();
+			System.out.flush();
+		}
+
+		System.err.println(iss.frontCount);
+	}
+
+	private Status status = new Status(INITIAL_POSITION, MINUMUM_SPEED);
+	private int frontCount = 0;
+
+	public double getInitialOrientation(double beta) {
+		return 3.0;
+	}
+
+	public double[] getStateAtMinute(int minute) {
+		Status nextStatus = null;
+		if (minute == 0) {
+			nextStatus = status.computeNextFromCommandVector(
+				SARJCommand.FRONT_HALF, SARJCommand.FRONT_HALF,
+				BGACommand.FRONT_HALF, BGACommand.FRONT_HALF,
+				BGACommand.FRONT_HALF, BGACommand.FRONT_HALF,
+				BGACommand.FRONT_HALF, BGACommand.FRONT_HALF,
+				BGACommand.FRONT_HALF, BGACommand.FRONT_HALF);
+			frontCount++;
+		}
+		if (minute > 0 && minute < 20) {
+			nextStatus = status.computeNextFromCommandVector(
+				SARJCommand.FRONT, SARJCommand.FRONT,
+				BGACommand.FRONT, BGACommand.FRONT,
+				BGACommand.FRONT, BGACommand.FRONT,
+				BGACommand.FRONT, BGACommand.FRONT,
+				BGACommand.FRONT, BGACommand.FRONT);
+			frontCount++;
+		}
+		if (minute == 20) {
+			nextStatus = status.computeNextFromCommandVector(
+				SARJCommand.FRONT_HALF, SARJCommand.FRONT_HALF,
+				BGACommand.FRONT_HALF, BGACommand.FRONT_HALF,
+				BGACommand.FRONT_HALF, BGACommand.FRONT_HALF,
+				BGACommand.FRONT_HALF, BGACommand.FRONT_HALF,
+				BGACommand.FRONT_HALF, BGACommand.FRONT_HALF);
+			frontCount++;
+		}
+		if (minute > 20 && minute < 40) {
+			nextStatus = status.computeNextFromCommandVector(
+				SARJCommand.STOP, SARJCommand.STOP,
+				BGACommand.STOP, BGACommand.STOP,
+				BGACommand.STOP, BGACommand.STOP,
+				BGACommand.STOP, BGACommand.STOP,
+				BGACommand.STOP, BGACommand.STOP);
+		}
+		if (minute == 40) {
+			nextStatus = status.computeNextFromCommandVector(
+				SARJCommand.BACK_HALF, SARJCommand.BACK_HALF,
+				BGACommand.BACK_HALF, BGACommand.BACK_HALF,
+				BGACommand.BACK_HALF, BGACommand.BACK_HALF,
+				BGACommand.BACK_HALF, BGACommand.BACK_HALF,
+				BGACommand.BACK_HALF, BGACommand.BACK_HALF);
+			frontCount++;
+		}
+		if (minute > 40 && minute < 60) {
+			nextStatus = status.computeNextFromCommandVector(
+				SARJCommand.BACK, SARJCommand.BACK,
+				BGACommand.BACK, BGACommand.BACK,
+				BGACommand.BACK, BGACommand.BACK,
+				BGACommand.BACK, BGACommand.BACK,
+				BGACommand.BACK, BGACommand.BACK);
+			frontCount--;
+		}
+		if (minute == 60) {
+			nextStatus = status.computeNextFromCommandVector(
+				SARJCommand.BACK_HALF, SARJCommand.BACK_HALF,
+				BGACommand.BACK_HALF, BGACommand.BACK_HALF,
+				BGACommand.BACK_HALF, BGACommand.BACK_HALF,
+				BGACommand.BACK_HALF, BGACommand.BACK_HALF,
+				BGACommand.BACK_HALF, BGACommand.BACK_HALF);
+			frontCount++;
+		}
+		if (minute > 60 && minute < 80) {
+			nextStatus = status.computeNextFromCommandVector(
+				SARJCommand.STOP, SARJCommand.STOP,
+				BGACommand.STOP, BGACommand.STOP,
+				BGACommand.STOP, BGACommand.STOP,
+				BGACommand.STOP, BGACommand.STOP,
+				BGACommand.STOP, BGACommand.STOP);
+		}
+		if (minute >= 80) {
+			nextStatus = status.computeNextFromPositionVector(
+				0.0, 0.0,
+				0.0, 0.0,
+				0.0, 0.0,
+				0.0, 0.0,
+				0.0, 0.0
+			);
+		}
+
+		double[] retour = status.getStatus();
+
+		status = nextStatus;
+
+		return retour;
+	}
+
+	public static class Speeds {
+		private final double[] speeds;
+
+		public Speeds(double... values) {
+			this.speeds = values;
+		}
+
+		public String toString() {
+			StringBuilder toString = new StringBuilder();
+			for (double value : speeds) {
+				toString.append(roundedDouble(value)).append(" ");
+			}
+			return toString.toString();
+		}
+	}
+
+	public static class Positions {
+		private final double[] positions;
+
+		private Positions(Positions previousPosition, Speeds previousSpeeds, SARJCommand sarjA, SARJCommand sarjB, BGACommand... bgaCommands) {
+			this(new double[10]);
+
+			positions[0] = previousPosition.positions[0] + sarjA.getSpeed(previousSpeeds.speeds[0]) * 60;
+			positions[1] = previousPosition.positions[1] + sarjB.getSpeed(previousSpeeds.speeds[1]) * 60;
+
+			for (int i = 2; i < 10; i++) {
+				positions[i] = limitAngularPosition((previousPosition.positions[i] + bgaCommands[i-2].getSpeed(previousSpeeds.speeds[i-2])) * 60);
+			}
+		}
+
+		public Positions(double... values) {
+			this.positions = values;
+		}
+
+		private Speeds computeSpeedsForNew(Positions newPosition) {
+			double[] resultingValues = new double[10];
+			for (int i = 0; i < 2; i++) {
+				resultingValues[i] = speed(positions[i], newPosition.positions[i], SARJ_VELOCITY_LIMIT);
+			}
+			for (int i = 2; i < 10; i++) {
+				resultingValues[i] = speed(positions[i], newPosition.positions[i], BGA_VELOCITY_LIMIT);
+			}
+			return new Speeds(resultingValues);
+		}
+
+
+		public String toString() {
+			StringBuilder toString = new StringBuilder();
+			for (double value : positions) {
+				toString.append(roundedDouble(value)).append(" ");
+			}
+			return toString.toString();
+		}
+	}
+
+	public static class Status {
+		private final Positions positions;
+		private final Speeds speeds;
+
+		public Status(Positions initialPositions, Speeds initialSpeeds) {
+			this.positions = initialPositions;
+			this.speeds = initialSpeeds;
+		}
+
+		public Status computeNextFromCommandVector(SARJCommand sarjA, SARJCommand sarjB, BGACommand... bgaCommands) {
+			Positions nextPositions = new Positions(positions, speeds, sarjA, sarjB, bgaCommands);
+			Speeds nextSpeeds = positions.computeSpeedsForNew(nextPositions);
+
+			return new Status(nextPositions, nextSpeeds);
+		}
+
+		public Status computeNextFromPositionVector(double... angularPositions) {
+			Positions nextPositions = new Positions(
+				transition(positions.positions[0], angularPositions[0], SARJ_VELOCITY_LIMIT * 60),
+				transition(positions.positions[1], angularPositions[1], SARJ_VELOCITY_LIMIT * 60),
+				transition(positions.positions[2], angularPositions[2], BGA_VELOCITY_LIMIT * 60),
+				transition(positions.positions[3], angularPositions[3], BGA_VELOCITY_LIMIT * 60),
+				transition(positions.positions[4], angularPositions[4], BGA_VELOCITY_LIMIT * 60),
+				transition(positions.positions[5], angularPositions[5], BGA_VELOCITY_LIMIT * 60),
+				transition(positions.positions[6], angularPositions[6], BGA_VELOCITY_LIMIT * 60),
+				transition(positions.positions[7], angularPositions[7], BGA_VELOCITY_LIMIT * 60),
+				transition(positions.positions[8], angularPositions[8], BGA_VELOCITY_LIMIT * 60),
+				transition(positions.positions[9], angularPositions[9], BGA_VELOCITY_LIMIT * 60)
+			);
+			Speeds nextSpeeds = positions.computeSpeedsForNew(nextPositions);
+
+			return new Status(nextPositions, nextSpeeds);
+		}
+
+		public double[] getStatus() {
+			return new double[] {
+				positions.positions[0],
+				speeds.speeds[0],
+				positions.positions[1],
+				speeds.speeds[1],
+				positions.positions[2],
+				speeds.speeds[2],
+				positions.positions[3],
+				speeds.speeds[3],
+				positions.positions[4],
+				speeds.speeds[4],
+				positions.positions[5],
+				speeds.speeds[5],
+				positions.positions[6],
+				speeds.speeds[6],
+				positions.positions[7],
+				speeds.speeds[7],
+				positions.positions[8],
+				speeds.speeds[8],
+				positions.positions[9],
+				speeds.speeds[9]
+			};
+		}
+
+		public String toString() {
+			return "Positions:"+positions+", Speeds:"+speeds;
+		}
+	}
+
+	public static enum SARJCommand {
+		FRONT(SARJ_VELOCITY_LIMIT), FRONT_HALF(SARJ_VELOCITY_HALF), STOP(0.0), BACK_HALF(-SARJ_VELOCITY_HALF), BACK(-SARJ_VELOCITY_LIMIT);
+
+		private final double commandSpeed;
+
+		SARJCommand(double commandSpeed) {
+			this.commandSpeed = commandSpeed;
+		}
+
+		public double getSpeed(double previousSpeed) {
+			return commandSpeed;
+		}
+	}
+
+	public static enum BGACommand {
+		FRONT(BGA_VELOCITY_LIMIT), FRONT_HALF(BGA_VELOCITY_HALF), STOP(0.0), BACK_HALF(-BGA_VELOCITY_HALF), BACK(-BGA_VELOCITY_LIMIT);
+
+		private final double commandSpeed;
+
+		BGACommand(double commandSpeed) {
+			this.commandSpeed = commandSpeed;
+		}
+
+		public double getSpeed(double previousSpeed) {
+			return commandSpeed;
+		}
+	}
+
+	public static String roundedDouble(double inputValue) {
+		double value = Math.abs(inputValue);
+		double signum = Math.signum(inputValue);
+
+		long left = (long) value;
+		long right = Math.round(100000 * (value - left)) / 100;
+		String padding = "";
+		if (right < 10) {
+			padding += "0";
+		}
+		if (right < 100) {
+			padding += "0";
+		}
+		return (signum < 0.0 ? "-" : "") + left + "." + padding + right;
+	}
+
+	public static double limitAngularPosition(double value) {
+		double modulo = value % 360;
+		return modulo >= 0.0 ? modulo : 360 + modulo;
+	}
+
+	public static double transition(double from, double to, double maximumSpeed) {
+		from = limitAngularPosition(from);
+		to = limitAngularPosition(to);
+
+		double delta = angle(from, to);
+		double signum = Math.signum(delta);
+		double speed = Math.min(maximumSpeed, Math.abs(delta));
+
+		return limitAngularPosition(from + signum * speed);
+	}
+
+	public static double speed(double from, double to, double maximumSpeed) {
+		from = limitAngularPosition(from);
+		to = limitAngularPosition(to);
+
+		double delta = angle(from, to);
+		double signum = Math.signum(delta);
+		double speed = Math.min(maximumSpeed, Math.abs(delta / 60));
+
+		return signum * speed;
+	}
+
+	public static double angle(double a, double b) {
+		double delta = limitAngularPosition(b - a);
+
+		if (delta > 180) {
+			return delta - 360;
+		}
+
+		return delta;
+	}
+}
---
+++ src/ISSTest.java
@@ -0,0 +1,68 @@
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class ISSTest {
+	@Test
+	public void shouldRoundDoublesCorrectly() {
+		assertThat(ISS.roundedDouble(10.2)).isEqualTo("10.200");
+		assertThat(ISS.roundedDouble(10.1234)).isEqualTo("10.123");
+		assertThat(ISS.roundedDouble(10.9876)).isEqualTo("10.987");
+		assertThat(ISS.roundedDouble(360.0)).isEqualTo("360.000");
+		assertThat(ISS.roundedDouble(0.001)).isEqualTo("0.001");
+		assertThat(ISS.roundedDouble(-0.001)).isEqualTo("-0.001");
+		assertThat(ISS.roundedDouble(-500.0)).isEqualTo("-500.000");
+	}
+
+	@Test
+	public void shouldLimitAngularPositions() {
+		assertThat(ISS.limitAngularPosition(361.0)).isEqualTo(1.0);
+		assertThat(ISS.limitAngularPosition(2*360+1.0)).isEqualTo(1.0);
+		assertThat(ISS.limitAngularPosition(-1.0)).isEqualTo(359.0);
+		assertThat(ISS.limitAngularPosition(-354.0)).isEqualTo(6.0);
+	}
+
+	@Test
+	public void shouldComputeAngles() {
+		assertThat(ISS.angle(10, 10)).isEqualTo(0.0);
+		assertThat(ISS.angle(10, 20)).isEqualTo(10.0);
+		assertThat(ISS.angle(20, 10)).isEqualTo(-10.0);
+		assertThat(ISS.angle(355, 5)).isEqualTo(10.0);
+		assertThat(ISS.angle(5, 355)).isEqualTo(-10.0);
+	}
+
+	@Test
+	public void shouldTransition() {
+		assertThat(ISS.transition(0.0, 1.0, Double.POSITIVE_INFINITY)).isEqualTo(1.0);
+		assertThat(ISS.transition(1.0, 0.0, Double.POSITIVE_INFINITY)).isEqualTo(0.0);
+		assertThat(ISS.transition(359.0, 0.0, Double.POSITIVE_INFINITY)).isEqualTo(0.0);
+		assertThat(ISS.transition(0.0, 359.0, Double.POSITIVE_INFINITY)).isEqualTo(359.0);
+
+		assertThat(ISS.transition(0.0, 1.0, 0.5)).isEqualTo(0.5);
+		assertThat(ISS.transition(1.0, 0.0, 0.5)).isEqualTo(0.5);
+		assertThat(ISS.transition(359.0, 0.0, 0.5)).isEqualTo(359.5);
+		assertThat(ISS.transition(0.0, 359.0, 0.5)).isEqualTo(359.5);
+	}
+
+	@Test
+	public void shouldSpeed() {
+		assertThat(ISS.speed(0.0, 1.0, Double.POSITIVE_INFINITY)).isEqualTo(1.0 / 60);
+		assertThat(ISS.speed(1.0, 0.0, Double.POSITIVE_INFINITY)).isEqualTo(-1.0 / 60);
+		assertThat(ISS.speed(359.0, 0.0, Double.POSITIVE_INFINITY)).isEqualTo(1.0 / 60);
+		assertThat(ISS.speed(0.0, 359.0, Double.POSITIVE_INFINITY)).isEqualTo(-1.0 / 60);
+
+		assertThat(ISS.speed(0.0, 100.0, 0.5)).isEqualTo(0.5);
+		assertThat(ISS.speed(100.0, 0.0, 0.5)).isEqualTo(-0.5);
+		assertThat(ISS.speed(300.0, 0.0, 0.5)).isEqualTo(0.5);
+		assertThat(ISS.speed(0.0, 300.0, 0.5)).isEqualTo(-0.5);
+	}
+
+	@Test
+	public void shouldHandleSARJCommands() {
+		assertThat(ISS.SARJCommand.FRONT.getSpeed(0.0)).isEqualTo(ISS.SARJ_VELOCITY_LIMIT);
+		assertThat(ISS.SARJCommand.BACK.getSpeed(0.0)).isEqualTo(-ISS.SARJ_VELOCITY_LIMIT);
+
+		assertThat(ISS.SARJCommand.FRONT.getSpeed(ISS.SARJ_VELOCITY_LIMIT)).isEqualTo(ISS.SARJ_VELOCITY_LIMIT);
+		assertThat(ISS.SARJCommand.BACK.getSpeed(ISS.SARJ_VELOCITY_LIMIT)).isEqualTo(-ISS.SARJ_VELOCITY_LIMIT);
+	}
+}
