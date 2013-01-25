14bec195b4a1fc562eaf13e4c19809fc73e0a192
Mathieu Bolla <mbolla@splayce.com>
2013-01-25 11:52:32 +0100

    Better bench

--- bench.sh
+++ bench.sh
@@ -5,7 +5,6 @@ for beta in "-72" "-71" "71" "72"
 do
     javac src/ISS.java && java -jar resources/ISSVis.jar -exec "java -cp src/ ISS" -model resources/ISS_simple.model -beta $beta > results.dat
     compute="$sum + `grep \"Score\" results.dat | cut -d " " -f 3`"
-    echo $compute
     sum=`echo "$compute" | bc`
 done
 echo "$sum / 4" | bc
