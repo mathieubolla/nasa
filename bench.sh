d7b656d2e1e440606eb9565aff26f347358e4534
Mathieu Bolla <mbolla@splayce.com>
2013-01-29 15:34:59 +0100

    Make a parallel bench

--- bench.sh
+++ bench.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-sum="0.0"
-#for beta in "-72" "-71" "71" "72"
+javac src/ISS.java
+rm -rf results:*.dat
+
 for beta in "70" "71" "72" "73" "74" "75" "-70" "-71" "-72" "-73" "-74" "-75"
 do
-    javac src/ISS.java && java -jar resources/ISSVis.jar -exec "java -cp src/ ISS" -model resources/ISS_simple.model -beta $beta > results.dat
-    compute="$sum + `grep \"Score\" results.dat | cut -d " " -f 3`"
+    java -jar resources/ISSVis.jar -exec "java -cp src/ ISS" -model resources/ISS_simple.model -beta $beta > results:${beta}.dat &
+done
+wait
+
+sum="0.0"
+for beta in "70" "71" "72" "73" "74" "75" "-70" "-71" "-72" "-73" "-74" "-75"
+do
+	compute="$sum + `grep \"Score\" results:${beta}.dat | cut -d " " -f 3`"
     sum=`echo "$compute" | bc`
 done
+
 echo "$sum / 12" | bc
 rm -rf results.dat
